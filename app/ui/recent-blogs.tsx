4b230b6ee2ec912b37cf368efa9c8f5a8884ce3d
starlightroad <ortizge92@gmail.com>
2024-07-19 17:51:47 -0400

    Add recent blog posts to home page

--- app/ui/recent-blogs.tsx
+++ app/ui/recent-blogs.tsx
@@ -1,9 +1,30 @@
-export default function RecentBlogs() {
+import Link from "next/link";
+import { getBlogPosts } from "@/app/lib/data";
+
+export default async function RecentBlogs() {
+  const blogs = await getBlogPosts();
+  const filteredBlogs = blogs.slice(0, 3);
+
   return (
     <section>
       <article className="grid gap-4">
         <h2 className="mb-2 text-xl font-medium text-gray-900">Recent Blogs</h2>
-        <p className="text-gray-600">Something great is on the way.</p>
+        {filteredBlogs.map((blog) => {
+          const { publishedAt, title } = blog.metadata;
+          const id = `blog-${blog.slug}`;
+
+          const publicationDate = new Date(publishedAt).toISOString().split("T")[0];
+          const location = `/blog/${title.toLowerCase().split(" ").join("-")}`;
+
+          return (
+            <article key={id}>
+              <Link href={location} className="-mx-3 flex gap-4 rounded-xl p-3 hover:bg-gray-50">
+                <p className="text-gray-600">{publicationDate}</p>
+                <h2 className="font-medium text-gray-900">{title}</h2>
+              </Link>
+            </article>
+          );
+        })}
       </article>
     </section>
   );
