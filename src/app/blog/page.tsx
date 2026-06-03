271fa5ba98ef628d69ef9a0ddeda93f9a9691fd0
starlightroad <ortizge92@gmail.com>
2026-06-02 22:18:36 -0400

    Build the blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <main className="pb-10">
+      <h1 className="text-foreground mt-10 text-2xl font-medium">Articles</h1>
+      <p className="text-muted-foreground mt-5">Content is on its way. Stay tuned!</p>
+    </main>
+  );
+}
