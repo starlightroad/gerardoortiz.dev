ab91736b2a9f2857fea0f95c53f1be1970cbbf31
starlightroad <ortizge92@gmail.com>
2026-06-05 18:56:04 -0400

    Add / to site logo path to load logo from public folder

--- src/components/page/site-logo.tsx
+++ src/components/page/site-logo.tsx
@@ -9,7 +9,7 @@ import { getSiteLogoByTheme } from "@/lib/utils";
 export default function SiteLogo() {
   const { resolvedTheme } = useTheme();
 
-  const siteLogo = getSiteLogoByTheme(resolvedTheme);
+  const siteLogo = `/${getSiteLogoByTheme(resolvedTheme)}`;
 
   return <Image src={siteLogo} width={20} height={20} alt="Gerardo Ortiz's site logo." />;
 }
